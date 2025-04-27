be5faec611dbc157530ede5c07e08963373f5187
sakibahmedshanto <sakibsanto57@gmail.com>
2025-04-27 13:04:31 +0600

    completed the alert system and fixed some errors in student data

--- UniSync/src/main/java/com/example/UniSync/StudentData.java
+++ UniSync/src/main/java/com/example/UniSync/StudentData.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.UniSync;
 
 import java.sql.Date;
@@ -11,142 +7,72 @@ import java.sql.Date;
  *
  * @author WINDOWS 10
  */
-public class StudentData {
+public class DataStudentHandle {
 
-    private Integer id;
+    private String teacherID;
     private String studentID;
-    private String fullName;
+    private String name;
     private String gender;
-    private Date birthDate;
-    private String year;
     private String course;
-    private String section;
-    private Double payment;
-    private String statusPayment;
-    private String image;
-    private int age;
-
+    private String year;
+    private String semester;
     private Date dateInsert;
-    private Date dateUpdate;
     private Date dateDelete;
     private String status;
-    private String semester;
 
-    public StudentData(Integer id, String studentID, String fullName, String gender,
-                       Date birthDate, int age, String year, String course, String section, String semester,
-                       Double payment, String statusPayment, String image, Date dateInsert,
-                       Date dateUpdate, Date dateDelete, String status) {
-        this.id = id;
+    public DataStudentHandle(String studentID, String name, String gender,
+                             String course, String year, String semester, Date dateInsert,
+                             Date dateDelete, String status) {
         this.studentID = studentID;
-        this.fullName = fullName;
+        this.name = name;
         this.gender = gender;
-        this.birthDate = birthDate;
-        this.age = age;
-        this.year = year;
         this.course = course;
-        this.section = section;
+        this.year = year;
         this.semester = semester;
-        this.payment = payment;
-        this.statusPayment = statusPayment;
-        this.image = image;
         this.dateInsert = dateInsert;
-        this.dateUpdate = dateUpdate;
         this.dateDelete = dateDelete;
         this.status = status;
     }
 
-    public StudentData(Integer id, String studentID, String fullName, String year, String course,
-                       String section, String semester, Double payment, String statusPayment, Date dateInsert, String status) {
-        this.id = id;
+    public DataStudentHandle(String teacherID, String studentID
+            , String name, String gender,  Date dateInsert) {
+        this.teacherID = teacherID;
         this.studentID = studentID;
-        this.fullName = fullName;
-        this.year = year;
-        this.course = course;
-        this.section = section;
-        this.semester = semester;
-        this.payment = payment;
-        this.statusPayment = statusPayment;
+        this.name = name;
+        this.gender = gender;
         this.dateInsert = dateInsert;
-        this.status = status;
     }
 
-    public StudentData(Integer id, String studentID, String fullName, String year, String course,
-                       String section, String semester, Double payment, String statusPayment, String image,
-                       Date dateUpdate, String status) {
-        this.id = id;
-        this.studentID = studentID;
-        this.fullName = fullName;
-        this.year = year;
-        this.course = course;
-        this.section = section;
-        this.semester = semester;
-        this.payment = payment;
-        this.statusPayment = statusPayment;
-        this.image = image;
-        this.dateUpdate = dateUpdate;
-        this.status = status;
-    }
-
-
-
-    public Integer getId() {
-        return id;
+    public String getTeacherID() {
+        return teacherID;
     }
 
     public String getStudentID() {
         return studentID;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getName() {
+        return name;
     }
 
     public String getGender() {
         return gender;
     }
 
-    public Date getBirthDate() {
-        return birthDate;
+    public String getCourse() {
+        return course;
     }
 
     public String getYear() {
         return year;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
     public String getSemester() {
         return semester;
     }
 
-    public String getSection() {
-        return section;
-    }
-
-    public Double getPayment() {
-        return payment;
-    }
-
-    public String getStatusPayment() {
-        return statusPayment;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
     public Date getDateInsert() {
         return dateInsert;
-    }
-
-    public Date getDateUpdate() {
-        return dateUpdate;
     }
 
     public Date getDateDelete() {
@@ -156,4 +82,5 @@ public class StudentData {
     public String getStatus() {
         return status;
     }
+
 }

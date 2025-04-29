8ff32bfd4bfd86cca0c501dd21ab1a6058a557bf
sakibahmedshanto <sakibsanto57@gmail.com>
2025-04-29 14:42:03 +0600

    created the skeleton for the subject object with necessary methods prototype

--- UniSync/src/main/java/com/example/UniSync/DataSubjectHandle.java
+++ UniSync/src/main/java/com/example/UniSync/DataSubjectHandle.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.UniSync;
 
 import java.sql.Date;
 
 /**
- *
  * @author WINDOWS 10
  */
 public class DataSubjectHandle {
@@ -49,3 +44,5 @@ public class DataSubjectHandle {
     }
 
 }
+
+
